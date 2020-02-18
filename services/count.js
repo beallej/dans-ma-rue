37a68cee2206bd6d46245cd12647a0e1971f7743
Josie Bealle <josiebealle@gmail.com>
2020-02-18 10:32:15 +0100

    Josie | count geo

--- services/count.js
+++ services/count.js
@@ -24,11 +24,37 @@ exports.count = (client, from, to, callback) => {
     });
 
 
-}
+};
 
 exports.countAround = (client, lat, lon, radius, callback) => {
-    // TODO Compter le nombre d'anomalies autour d'un point géographique, dans un rayon donné
-    callback({
-        count: 0
-    })
+
+    client.count({
+        index: indexName,
+        body : {
+            query: {
+                bool: {
+                    must: {
+                        match_all: {}
+                    },
+                    filter: {
+                        geo_distance: {
+                            distance: radius,
+                            location: {
+                                lat: lat,
+                                lon: lon
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }).then(function(resp) {
+        console.log("Successful query!");
+        console.log(JSON.stringify(resp, null, 4));
+        callback({
+            count: resp.body.count
+        })
+    }, function(err) {
+        console.trace(err.message);
+    });
 }
